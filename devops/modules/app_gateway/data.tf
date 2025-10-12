2c024a271fafa67bf527497feec7973e04589be3
Abdullah010 <166231259+omega0100@users.noreply.github.com>
2025-10-12 07:11:31 +0300

    Update data.tf
    23434

--- devops/modules/app_gateway/data.tf
+++ devops/modules/app_gateway/data.tf
@@ -1,3 +1,4 @@
+#fff
 data "azurerm_log_analytics_workspace" "ws" {
   count               = (var.log_analytics_workspace_name != null && var.log_analytics_workspace_rg != null) ? 1 : 0
   name                = var.log_analytics_workspace_name
